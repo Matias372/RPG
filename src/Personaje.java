f9d9b76c283b2eb71cf9db36fb31fc753507ef21
Matias372 <matiasruibal372@gmail.com>
2022-08-31 10:31:00 -0300

    se modifica Mover();

--- src/Personaje.java
+++ src/Personaje.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class Personaje {
 
-	String Ubicacion="Ciudad"; // Ciudad / Mazmorra
-	int Piso=0;// piso de mazmorra, si esta en ciudad ser cero.
-	String Nombre;
-	boolean Salir = false;
-	boolean Seguro = true; // Ciudad=True / Masmorra= False.  usar estado para verificar si se puede dormir
+	private String Ubicacion="Ciudad"; // Ciudad / Mazmorra
+	private int Piso=0;// piso de mazmorra, si esta en ciudad ser cero.
+	public String Nombre;
+	public boolean Salir = false;
+	public boolean Seguro = true; // Ciudad=True / Masmorra= False.  usar estado para verificar si se puede dormir
 	//ESTADISTICAS
 	int Vida = 100;
 	int VidaMax = 100; //vida maximo
@@ -17,8 +17,8 @@ public class Personaje {
 	int Defensa = 5;
 	int Inteligencia = 5;
 	//variables extra
-	float Dinero = 0;
-	float Experiencia =0;
+	private float Dinero = 0;
+	private float Experiencia =0;
 	// agregar variable clase
 	
 	//ACCIONES
@@ -42,7 +42,7 @@ public class Personaje {
 			case "VER PERSONAJE":
 				System.out.println("EN CONSTRUCCION...");
 				// generar clase equipo, clase inventario, acomodar estadisticas.
-				Accion();
+				//vuelve solo a Accion();
 				break;
 			case "MOVER":
 				Mover(); //modificar funcion. dependiendo de ubicacion cambiar menu (colocar -Tienda -Explorar ciudad -Mazmorra -Atras)
@@ -52,7 +52,7 @@ public class Personaje {
 				break;
 				default:
 					System.out.println("No se entendio la respuesta.");
-					Accion();
+					//vuelve solo a Accion();
 					break;
 			}
 		}
@@ -65,12 +65,12 @@ public class Personaje {
 			case "EXPLORAR":
 				System.out.println("EN CONSTRUCCION...");
 				// generar Accion Explorar
-				Accion();
+				//vuelve solo a Accion();
 				break;
 			case "VER PERSONAJE":
 				System.out.println("EN CONSTRUCCION...");
 				// generar clase equipo, clase inventario, acomodar estadisticas.
-				Accion();
+				//vuelve solo a Accion();
 				break;
 			case "MOVER":
 				Mover(); //modificar funcion. dependiendo de ubicacion cambiar menu (colocar -Piso -Ciudad -Atras)
@@ -80,7 +80,7 @@ public class Personaje {
 				break;
 				default:
 					System.out.println("No se entendio la respuesta.");
-					Accion();
+					//vuelve solo a Accion();
 					break;
 			}
 		}
@@ -105,14 +105,15 @@ public class Personaje {
 				Vida = VidaMax;
 				Mana = ManaMax;
 				System.out.println("...Recuperaste la salud y mana al dormir.");
+				//vuelve solo a Accion();
 			}else {
 				System.out.println("No es posible dormir en la ubicacion actual");
-				Accion();
+				//vuelve solo a Accion();
 			}
 			break;
 			
 		case "NO":
-			Accion();
+			//vuelve solo a Accion();
 			break;
 			
 		default:
@@ -140,15 +141,24 @@ public class Personaje {
 			switch (decision) {
 			
 			case "Tienda":
+				System.out.println("EN CONSTRUCCION...");
+				//vuelve solo a Accion();
 				break;
 				
 			case "Explorar Ciudad":
+				System.out.println("EN CONSTRUCCION...");
+				//vuelve solo a Accion();
 				break;
 				
 			case "Mazmorra":
+				System.out.println("Te diriges a la mazmorra.");
+				Ubicacion= "Mazmorra";
+				Piso= 1;
+				//vuelve solo a Accion();
 				break;
 				
 			case "Atras":
+				//vuelve solo a Accion();
 				break;
 			
 			default:
@@ -159,42 +169,68 @@ public class Personaje {
 		}
 		else if(Ubicacion =="Mazmorra") {
 			
-		}
-		
-		
-		/*
-		Scanner Respuesta = new Scanner(System.in);
-		
-		System.out.println("Estas en la " + Ubicacion + ". Donde deseas ir?");
-		System.out.println("-Ciudad \n-Mazmorra");
-		String decision = Respuesta.nextLine();
-		
-		
-		switch (decision) {
-		
-			case "Ciudad":
-				
-				Ubicacion = "Ciudad";
-				Seguro = true;
-				System.out.println("Estas en la " + Ubicacion);
-				Accion();
+			System.out.println("Estas en la Mazmorra en el piso " + Piso + ". Donde deseas ir?");
+			System.out.println("-Piso\t-Ciudad\n-Atras");
+			String decision = Respuesta.nextLine();
+			
+			switch (decision) {
+			
+				case "Piso":
+					if(Piso == 1) {
+						System.out.println("Estas en el piso " + Piso);
+						System.out.println("-Piso" + (Piso++) + "\t-Atras");
+						String decision2 = Respuesta.nextLine();
+							//SE USA IF YA QUE EL SWITCH NO TOMA "Piso " + (Piso++).
+							if (decision2 == "Piso " + (Piso++)) {
+								System.out.println("Bajas las escaleras.");
+								Piso++;
+								//vuelve solo a Accion();
+							}else if(decision2 != "Atras" && decision2 != "Piso " + (Piso++)) {
+								System.out.println("No se entendio la respuesta.");
+								Mover();
+								
+							}
+					}else if(Piso > 1) {
+						System.out.println("Estas en el piso " + Piso);
+						System.out.println("-Piso" + (Piso--) +"\t-Piso" + (Piso++) + "\n-Atras");
+						String decision2 = Respuesta.nextLine();
+							//SE USA IF YA QUE EL SWITCH NO TOMA "Piso " + (Piso++).
+							if (decision2 == "Piso " + (Piso--)) {
+								System.out.println("Subes las escaleras.");
+								Piso--;
+								//vuelve solo a Accion();
+							}else if(decision2 == "Piso " + (Piso++)) {
+								System.out.println("Bajas las escaleras.");
+								Piso++;
+							
+							}else if(decision2 != "Atras" && decision2 != "Piso " + (Piso++) && decision2 == "Piso " + (Piso--)) {
+								System.out.println("No se entendio la respuesta.");
+								Mover();
+								
+							}
+							
+					}
+					break;
 					
-				break;
+				case "Ciudad":
+					System.out.println("Te diriges a la ciudad.");
+					Ubicacion= "Ciudad";
+					Piso= 0;
+					//vuelve solo a Accion();
+					break;
+					
+				case "Atras":
+					//vuelve solo a Accion();
+					break;
 				
-			case "Mazmorra":
-				Ubicacion = "Mazmorra";
-				Seguro = false;
-				System.out.println("Estas en la " + Ubicacion);
-				Accion();
-				break;
-				
-			default:
-				System.out.println("No se entendio la respuesta.");
-				Mover();
-				break;
-		
+				default:
+					System.out.println("No se entendio la respuesta.");
+					Mover();
+					break;
+			}
 		}
-	*/	
+		
+		
 	}
 	//-------------------------------------------------------------------------------------------------------------------------
 	public void Explorar() {
@@ -218,7 +254,7 @@ public class Personaje {
 				break;
 				
 			case "NO":
-				Accion();
+				//vuelve solo a Accion();
 				break;
 				
 			default:
